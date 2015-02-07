166c3e6dfe3488c107537eaaca4f12310b9f2814
zulldesign <admin@zulldesign.ml>
2015-02-07 18:58:36 +0800

    l

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
-  echo 'Ini beneran pake PHP lho (in bahasa) <br/>';
-  echo 'Current PHP version: ' . phpversion();
-  
-  echo '<br/>Created by @tonidermawan';
-  //phpinfo();
+<!DOCTYPE html>
+<html>
+<body>
+
+<em>Emphasized text</em><br>
+<strong>Strong text</strong><br>
+<code>A piece of computer code</code><br>
+<samp>Sample output from a computer program</samp><br>
+<kbd>Keyboard input</kbd><br>
+<var>Variable</var>
+
+</body>
+</html>
 ?>
