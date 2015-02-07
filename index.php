8142b30971b8e864defa7fdcc73a73ed540316fd
zulldesign <admin@zulldesign.ml>
2015-02-07 19:30:55 +0800

    l

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-<em>Emphasized text</em><br>
-<strong>Strong text</strong><br>
-<code>A piece of computer code</code><br>
-<samp>Sample output from a computer program</samp><br>
-<kbd>Keyboard input</kbd><br>
-<var>Variable</var>
-?>
