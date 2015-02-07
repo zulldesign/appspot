50ccd746ebfa4be2e5a4795adbef008a9b57573e
zulldesign <admin@zulldesign.ml>
2015-02-07 19:08:21 +0800

    l

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
 <?php
-<!DOCTYPE html>
-<html>
-<body>
-
 <em>Emphasized text</em><br>
 <strong>Strong text</strong><br>
 <code>A piece of computer code</code><br>
 <samp>Sample output from a computer program</samp><br>
 <kbd>Keyboard input</kbd><br>
 <var>Variable</var>
-
-</body>
-</html>
 ?>
